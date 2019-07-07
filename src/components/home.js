171c3df5d868a29ead721d0dab4e0c4393f6913d
Stanley <stanleypl75@gmail.com>
2019-07-07 10:22:32 -0400

    Rename mission.js to home.js .

---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Home = ()=> (
+    <div>
+        <div id="home">
+            <div>
+            <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia iste nesciunt quasi, fugiat similique, quidem aliquam hic magnam, ipsam ab ipsum 
+            equi distinctio rerum facere incidunt consequatur dolor odio laborum?</p>
+            </div>
+            </div>
+    </div>
+);
+export default Home;
