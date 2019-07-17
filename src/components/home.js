16bd00e0ea84722bddccf7a86e1607370e816529
Stanley <stanleypl75@gmail.com>
2019-07-16 22:41:18 -0400

    Created the Home

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,27 @@
 import React from "react";
+import {FaDownload} from 'react-icons/fa';
+import './css/home.css';
+import logokay from "../logokay.jpg";
 
 
 const Home = ()=> (
     <div>
-        <div id="home">
-            <div>
-            <h2>Home</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia iste nesciunt quasi, fugiat similique, quidem aliquam hic magnam, ipsam ab ipsum 
+        <div className="home_container" id="home">
+            <div className="centered_Home">
+                <div className="image-container">
+                    <img className="logotwo" src={logokay } alt="logoTwo"/>
+                {/* <h2 className="title_home">Home</h2> */}
+                </div>
+            <div className="paragraph_home" >
+                {/* <h1>Home</h1> */}
+            <p className="paragraph"> <span className="begin_home">Lorem ipsum</span> dolor sit amet consectetur adipisicing elit. Quia iste nesciunt quasi, fugiat similique, quidem aliquam hic magnam, ipsam ab ipsum 
             equi distinctio rerum facere incidunt consequatur dolor odio laborum?</p>
+            <div className="btn-container">
+            <button className="btn-download"> <FaDownload/> Download Application</button>
+
+            </div>
+            </div>
+       
             </div>
             </div>
     </div>
