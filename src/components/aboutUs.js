444a8a8f4162a24038d4e804c73d78fe98c1de5f
Stanley <stanleypl75@gmail.com>
2019-09-25 15:33:52 -0400

    Add the title  and a paragraph

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -1,17 +1,23 @@
 import React from "react";
-import logo from '../logo.svg';
+import locationLogo from '../locationLogo.jpeg';
 import './css/aboutUs.css';
 
 const AboutUs=()=> (
     <div className="containerAbout" id="about">
-        <div>
-        <div className="logo">
-            <img  src={logo} alt="lo"/>
-            </div>
-            </div>
-        <div>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. , voluptas voluptatum vel assumenda nulla unde aut incidunt ut dicta corrupti exercitationem 
+          <h1 className="titleAbout">About us </h1>
+        <div className="centered_about">
+        <div className="paLoremragraph_about">
+        <p><span className="begin_about">Nou</span>  se Yon konpayi 100% Ayisyen ki la pou edew jwenn kay wap chache a nan nenpot kondisyon.
+Ou ka fé sa apati aplikasyon mobil nou yo oubyen sit wèb nou an.
+Ou gen posibilite pou chwazi tip:
+<span className="begin_about_one"> kay ou vle a,</span> <span className="begin_about_one"> zon ou vle chèche a e </span> <span className="begin_about_one"> pri kay ou teren kap bon pou ou a.</span> 
             </p>
+            </div>
+        <div className="containerAbout">
+            <img   className="logoabout" src={locationLogo} alt="locationLogo"/>
+            </div>
+            
+        
         </div>
     </div>
 );
