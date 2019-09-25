d841facaf73dae91f74b1be0cab6b47f2c16a2da
Stanley <stanleypl75@gmail.com>
2019-09-25 15:36:06 -0400

    Update the Contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,15 +1,40 @@
 import React from "react";
+import { FaHeadset,FaRegComments } from 'react-icons/fa';
+import "./css/contact.css";
 
-const Contact = ()=> (
-    <div id="contact">
-        <div>
-            <div>
-        <h2>Contact</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nam voluptatum incidunt in sequi modi, sunt ab veniam rem nostrum quae accusamus quia non sint dolores iusto? Id, vel dolorem.</p>
-        </div>
-        </div>
-        
-    </div>
-);
 
+function Contact (){
+return(
+    <div className="contact_container" id="contact">
+ <h1 className="titleContact">Contact Us </h1>
+   <div className="centered_card">
+        <div className="container_card">
+        <div className="card">
+    <span className="headset"> <FaHeadset /></span>
+  <div className="container">
+    <h2 className="subtitle_contact"><b>By Phone</b></h2>
+    <small>(Monday to Friday, 9am to 6pm PST)</small>
+					<br />
+					Phone 1:<br />
+					1-877-930-7483
+					<br></br >
+					Phone 2:<br />
+					1-604-637-0780
+				
+  </div>
+</div> 
+
+<div className="card">
+    <span className="headset"> <FaRegComments /></span>
+  <div className="container">
+    <h2 className="subtitle_contact"><b>Live Chat</b></h2>
+    <p className="liveChat">Chat with a member of our in-house team.</p>
+    <button className="btn-chat">Start Chat</button>
+  </div>
+</div> 
+</div>
+</div>
+</div>
+)
+}
 export default Contact;
