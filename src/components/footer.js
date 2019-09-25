9ecf969758487074a16ebcc81f99d17b274a519a
Stanley <stanleypl75@gmail.com>
2019-09-25 15:40:44 -0400

    Add icons list in the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,19 @@
 import React from "react";
+import {FaLinkedin,FaFacebook,FaTwitter,FaWhatsapp } from 'react-icons/fa';
+
 import './css/footer.css';
 
 const Footer = ()=> (
     <div className="Footer">
-      <div>
-        <p>Copyright © 2019 KayRapid</p>
+        <ul className="icons">
+        <li className="facebook"><FaFacebook/></li>
+        <li className="twitter"><FaTwitter/></li>
+        <li className="whatsapp"><FaWhatsapp/></li>
+        <li className="linkedin"><FaLinkedin/></li>
+        </ul>
+      
+      <div className="copyright">
+        <p>Copyright © 2019 KayRapid.com</p>
         </div>
     </div>
 );
