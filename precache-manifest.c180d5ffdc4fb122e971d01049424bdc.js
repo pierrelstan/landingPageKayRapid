825c8d98fa8f7dcada397307ab5c1a8b783b2ec0
Stanley <stanleypl75@gmail.com>
2019-09-28 21:58:20 -0400

    Updates

---
+++ precache-manifest.c180d5ffdc4fb122e971d01049424bdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e8cefaa563979bc78b7a78477a9652",
+    "url": "/landingPageKayRapid/index.html"
+  },
+  {
+    "revision": "514f88794772e19ce7b8",
+    "url": "/landingPageKayRapid/static/css/main.0e173a21.chunk.css"
+  },
+  {
+    "revision": "28aaccc60e96d916bcaf",
+    "url": "/landingPageKayRapid/static/js/2.77f37be4.chunk.js"
+  },
+  {
+    "revision": "514f88794772e19ce7b8",
+    "url": "/landingPageKayRapid/static/js/main.67a8cf2d.chunk.js"
+  },
+  {
+    "revision": "a7604ad046327eb1a73c",
+    "url": "/landingPageKayRapid/static/js/runtime~main.c63c95ea.js"
+  },
+  {
+    "revision": "d87d028ce0ebf0d9d7fa49bbd7965f1f",
+    "url": "/landingPageKayRapid/static/media/kayrapid.d87d028c.png"
+  },
+  {
+    "revision": "0f3ddc63c4520b8881ccfb677823cd77",
+    "url": "/landingPageKayRapid/static/media/locationLogo.0f3ddc63.jpeg"
+  },
+  {
+    "revision": "57139c0f2a1e31b56b9f3f2c07459474",
+    "url": "/landingPageKayRapid/static/media/logokay.57139c0f.jpg"
+  }
+]);
